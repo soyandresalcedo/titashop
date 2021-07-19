695238a8264312624b0e7f4750cf9a39bd33e434
soyandresalcedo <gandresalcedo09@gmail.com>
2021-07-18 20:45:53 -0500

    realizo cambios y agrego parametros para hacer deploy y autenticaciones con firebase

---
+++ public/js/auth/authController.js
@@ -0,0 +1,34 @@
+$(() => {
+  const objAuth = new Autenticacion()
+
+  $('#btnRegistroEmail').click(() => {
+    const nombres = $('#nombreContactoReg').val()
+    const email = $('#emailContactoReg').val()
+    const password = $('#passwordReg').val()
+    const auth =  new Autenticacion()
+    auth.crearCuentaEmailPass(email, password, nombres)
+  })
+
+  $('#btnInicioEmail').click(() => {
+    const email = $('#emailSesion').val()
+    const password = $('#passwordSesion').val()
+    const auth =  new Autenticacion()
+    auth.autEmailPass(email, password)
+  })
+
+  $('#authGoogle').click(() => objAuth.authCuentaGoogle())
+
+  $('#authFB').click(() => objAuth.authCuentaFacebook())
+
+  // $("#authTwitter").click(() => objAuth.authCuentaFacebook());
+
+  $('#btnRegistrarse').click(() => {
+    $('#modalSesion').modal('close')
+    $('#modalRegistro').modal('open')
+  })
+
+  $('#btnIniciarSesion').click(() => {
+    $('#modalRegistro').modal('close')
+    $('#modalSesion').modal('open')
+  })
+})
